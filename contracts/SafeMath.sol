64a4b7b09333ad3df7a3274a469fa9dce2aefa44
eosbet-io <development@eosbet.io>
2018-03-27 12:50:31 -0700

    update SafeMath to Zeppelin SafeMath, old one unnecessary

--- contracts/SafeMath.sol
+++ contracts/SafeMath.sol
@@ -1,37 +1,48 @@
 pragma solidity ^0.4.18;
 
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
 library SafeMath {
 
-  // modifying standard SafeMul to not take up another stack space -- eosbet comment
-
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
   function mul(uint256 a, uint256 b) internal pure returns (uint256) {
     if (a == 0) {
       return 0;
     }
-    // uint256 c = a * b; -- eosbet comment
-    assert((a * b) / a == b);
-    return a * b;
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
   }
 
-  // modifying standard SafeDiv to not take up another stack space -- eosbet comment
-
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
   function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0 -- comment in standard safemath library
-    // uint256 c = a / b; -- eosbet comment
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold -- comment in standard safemath library
-    return a / b;
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
   }
 
+  /**
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
   function sub(uint256 a, uint256 b) internal pure returns (uint256) {
     assert(b <= a);
     return a - b;
   }
 
-  // modifiying standard SafeAdd to not take up another stack space -- eosbet comment
-
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
   function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    // uint256 c = a + b; -- eosbet comment
-    assert((a + b) >= a);
-    return a + b;
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
   }
 }
